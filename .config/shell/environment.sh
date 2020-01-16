d321c48a541699f5de78e4b2582a20c2218f89a4
Henri Menke <henri@icp.uni-stuttgart.de>
2020-01-17 10:47:58 +1300

    [screen] use XDG dirs via env var

--- .config/shell/environment.sh
+++ .config/shell/environment.sh
@@ -39,3 +39,6 @@ export TERM=xterm-256color
 # Limit memory for processes to 80% of total RAM
 TOTAL_MEM=$(grep -e 'MemTotal:' /proc/meminfo | grep -oE '([0-9]+)')
 ulimit -Sv $((8 * TOTAL_MEM / 10))
+
+# make screen use XDG dir
+export SCREENRC="${HOME}/.config/screen/screenrc"
