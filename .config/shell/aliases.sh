eff2ec1647446b2694c2c8541cbdd1ab6de4cc09
Henri Menke <henri@icp.uni-stuttgart.de>
2020-02-16 20:54:42 +1300

    Manage systemsettings via package

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -45,7 +45,6 @@ alias gpgsign="gpg --armor --output '-' --detach-sig"
 
 # netsettings
 alias netsettings='git --git-dir="${HOME}/.netsettings/git" --work-tree="${HOME}"'
-alias systemsettings='git --git-dir="${HOME}/.netsettings/git/worktrees/system" --work-tree="/"'
 
 # Patch tmux to use XDG dirs
 if command -v tmux > /dev/null; then
