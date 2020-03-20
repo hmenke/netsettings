aa5eff5fff8b2ad5e2a716eaece71d403360bdc9
Henri Menke <henri@icp.uni-stuttgart.de>
2020-03-20 20:18:08 +1300

    [scripts] credentials.py: Support pass to retrieve netrc

--- .config/offlineimap/credentials.py
+++ .config/offlineimap/credentials.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 
-import netrc
+from io import StringIO
+from netrc import netrc
 from os.path import expanduser, isfile
-import sys
+from subprocess import check_output, PIPE
+from sys import argv
 
-filename = expanduser("~/.config/offlineimap/netrc")
 
-credentials = netrc.netrc(filename)
+credentials = netrc("/dev/null")
+file = expanduser("~/.config/offlineimap/netrc")
+if isfile(file):
+    with open(file) as fp:
+        credentials._parse(file, fp, True)
+else:
+    proc = check_output(["pass", "Accounts/GMail/mutt"])
+    with StringIO(proc.decode("utf-8")) as fp:
+        credentials._parse("pass", fp, False)
+
 
 def get_username(host):
     login, _, _ = credentials.hosts[host]
     return login
 
+
 def get_password(host):
     _, _, password = credentials.hosts[host]
     return password
 
+
 if __name__ == "__main__":
-    host = sys.argv[1]
+    host = argv[1]
     login = get_username(host)
     password = get_password(host)
     print("set imap_user = {}".format(login))
@@ -25,7 +37,7 @@ if __name__ == "__main__":
 
     # Decide whether to use the offline or online configuration
     if isfile(expanduser("~/.config/mutt/mbnames")):
-        print('set folder = ~/.local/share/offlineimap/GMail')
+        print("set folder = ~/.local/share/offlineimap/GMail")
         print('source "~/.config/mutt/mbnames"')
     else:
         print('set folder = "imaps://{}@imap.gmail.com/"'.format(login))
