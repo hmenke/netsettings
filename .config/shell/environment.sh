0c868f07f7f21de1ff55dc0f901a646e70f0bfa4
Henri Menke <henri@icp.uni-stuttgart.de>
2020-03-25 18:38:36 +1300

    [shell] More XDG compatibility

--- .config/shell/environment.sh
+++ .config/shell/environment.sh
@@ -36,8 +36,14 @@ export NO_AT_BRIDGE=1
 # 256 colors
 export TERM=xterm-256color
 
-# make screen use XDG dir
-export SCREENRC="${HOME}/.config/screen/screenrc"
+# XDG base directory support
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export SCREENRC="$XDG_CONFIG_HOME/screen/screenrc"
+export ANDROID_SDK_HOME="$XDG_DATA_HOME/share/android"
+export NOTMUCH_CONFIG="$XDG_CONFIG_HOME/notmuch/notmuchrc"
+export UNISON="$XDG_DATA_HOME/unison"
 
 # Colors in less (https://github.com/shibumi/hikari-zsh)
 export LESS_TERMCAP_mb="$(printf "\001\033[01;31m\002")"
@@ -47,3 +53,5 @@ export LESS_TERMCAP_se="$(printf "\001\033[0m\002")"
 export LESS_TERMCAP_so="$(printf "\001\033[01;44;33m\002")"
 export LESS_TERMCAP_ue="$(printf "\001\033[0m\002")"
 export LESS_TERMCAP_us="$(printf "\001\033[01;32m\002")"
+# Disable less history
+export LESSHISTFILE=-
