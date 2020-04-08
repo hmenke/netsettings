850790d710d6bd13c8b8b65f62fa66d639b9ffea
Henri Menke <henri@icp.uni-stuttgart.de>
2020-04-08 15:33:47 +1200

    [emacs] Add config for Gnus

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -148,6 +148,19 @@
                      (cons #'display-buffer-no-window nil))))
 (use-package notmuch
   :commands notmuch)
+(use-package gnus
+  :commands gnus
+  :init
+  (setq gnus-directory "~/.local/share/emacs/gnus"
+        message-directory "~/.local/share/emacs/gnus")
+  (setq gnus-select-method
+        '(nnimap "gmail"
+                 (nnimap-address "imap.gmail.com")
+                 (nnimap-server-port "imaps")
+                 (nnimap-stream ssl)))
+  (setq smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587)
+  (setq gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\”]\”[#’()]"))
 
 ; Theme
 (use-package base16-theme
