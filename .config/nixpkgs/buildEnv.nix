a7f92a75d6b7a986a7b28f33918d151fb88c4e51
Henri Menke <henrimenke@gmail.com>
2020-08-23 23:22:25 +1200

    [nix] import overlay from NixOS

--- .config/nixpkgs/buildEnv.nix
+++ .config/nixpkgs/buildEnv.nix
@@ -3,14 +3,18 @@
 # https://nixos.org/nixpkgs/manual/#sec-declarative-package-management
 # https://gist.github.com/lheckemann/402e61e8e53f136f239ecd8c17ab1deb
 #
-# Install using nix-env --set -f ~/.config/nixpkgs/buildEnv.nix
+# Install using
+#
+#    nix-env --set -f ~/.config/nixpkgs/buildEnv.nix
 
 let
 
+  config = import ./config.nix;
   overlays = import ./overlays.nix;
-  importChannel = channel: import (fetchTarball { url = "https://nixos.org/channels/${channel}/nixexprs.tar.xz"; });
-  pkgs = importChannel "nixos-20.03" { inherit overlays; };
-  unstable = importChannel "nixpkgs-unstable" { inherit overlays; };
+  nixexprs = fetchTarball {
+    url = "https://nixos.org/channels/nixos-20.03/nixexprs.tar.xz";
+  };
+  pkgs = import nixexprs { inherit config overlays; };
 
 in
 
@@ -22,21 +26,73 @@ buildEnv {
   inherit name;
   extraOutputsToInstall = [ "bin" "lib" "out" ];
   paths = [
-    nix # If not on NixOS, this is important!
-  ] ++ [
+    # command line utils
+    aspell
+    aspellDicts.de
+    aspellDicts.en
     cachix
     direnv
-    gnuplot_qt
+    ffmpeg
+    fzf
+    (hiPrio gcc)
+    gnuplotGit
+    isync
+    llvmPackages_latest.clang
+    msmtp
+    neomutt
+    newsboat
     niv
     nix-direnv
-    nixops
-    unstable.mathematica
-    vgo2nix
+    p7zip
+    pass-otp
+    qpdf
+    shadowsocks-libev
+    ts
+    unison
+    unzip
+    #v2ray-plugin
+    valgrind
+    youtube-dl
+    zip
   ] ++ [
+    # GUI
+    arandr
+    clementine
+    dunst
+    evince
+    feh
+    firefox
+    gnome3.adwaita-icon-theme
+    libnotify
+    networkmanagerapplet
+    pavucontrol
+    polybarFull
+    rofi
+    scrot
+    vlc
+    xclip
+    xorg.setxkbmap
+    xorg.xbacklight
+    xorg.xmodmap
+    xorg.xsetroot
+    xscreensaver
+  ] ++ [
+    # Proprietary
+    dropbox
+    google-chrome
+    mathematica
+    skypeforlinux
+    softmaker-office
+    zoom-us
+  ] ++
+  # Local package specifications (if they exist)
+  lib.lists.optional (builtins.pathExists ./local.nix) (import ./local.nix pkgs) ++
+  # Helpers for declarative package management
+  [
     # Script to rebuild the environment from this file.
     (writeScriptBin "nix-rebuild" ''
       #!${runtimeShell}
-      nix-env --set -f ~/.config/nixpkgs/buildEnv.nix --argstr name "$(whoami)-user-env-$(date -I)"
+      ${nix}/bin/nix-env --set -f ~/.config/nixpkgs/buildEnv.nix --argstr name "$(whoami)-user-env-$(date -I)"
     '')
 
     # Manifest to make sure imperative nix-env doesn't work (otherwise it will
@@ -53,6 +109,7 @@ buildEnv {
         ''\''
       '';
     })
+
     # To allow easily seeing which nixpkgs version the profile was built from,
     # place the version string in ~/.nix-profile/nixpkgs-version
     (writeTextFile {
--- .config/nixpkgs/overlays.nix
+++ .config/nixpkgs/overlays.nix
@@ -1,79 +1,82 @@
-[
-  #(import (builtins.fetchTarball {
-  #  url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-  #}))
+[ (self: super:
+let
+  config = import ./config.nix;
+  overlays = []; # no overlays inside overlay (infinite recursion)
+  nixexprs = fetchTarball {
+    url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
+  };
+  unstable = import nixexprs { inherit config overlays; };
+in {
+  inherit unstable;
 
-  (self: super: {
-    gnuplot_qt = super.gnuplot_qt.overrideAttrs (oldAttrs: {
-      version = "5.5+git-ef315e";
+  gnuplotGit = super.gnuplot_qt.overrideAttrs (oldAttrs: {
+    version = "5.5+git-ef315e";
 
-      patches = [ (self.writeText "transparency.patch"
-        ''
-          --- a/term/cairo.trm
-          +++ a/term/cairo.trm
-          @@ -849,7 +849,7 @@
-           	plot.background.g = cairo_params->background.g;
-           	plot.background.b = cairo_params->background.b;
-           	gp_cairo_set_background(cairo_params->background);
-          -	if (ISCAIROLATEX || cairo_params->transparent)
-          +	if (cairo_params->transparent)
-           		gp_cairo_clear_background(&plot);
-           	else
-           		gp_cairo_solid_background(&plot);
-        '') ];
-
-      src = self.fetchgit {
-        url = "https://git.code.sf.net/p/gnuplot/gnuplot-main";
-        rev = "ef315e6f148bb3144726246cbf396aad53e3a000";
-        sha256 = "143d3hbw8pk2pp3rpplr81ax5cbcrrfhz994zzky5xavdw4z43df";
-      };
-
-      nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-        self.autoconf
-        self.automake
-        self.git
-      ];
-
-      postPatch = ''
-        ./prepare
-        ${oldAttrs.postPatch}
-      '';
-    });
-
-    mathematica-unwrapped = let
-      version = "12.1.1";
-      name = "Mathematica_${version}_LINUX.sh";
-      sha256 = "02mk8gmv8idnakva1nc7r7mx8ld02lk7jgsj1zbn962aps3bhixd";
-    in super.mathematica.overrideAttrs (oldAttrs: {
-      name = "mathematica-unwrapped-${version}";
-      inherit version;
-      src = super.requireFile {
-        inherit name sha256;
-        message = ''
-          This nix expression requires that ${name} is
-          already part of the store. Find the file on your Mathematica CD
-          and add it to the nix store with nix-store --add-fixed sha256 <FILE>.
-        '';
-      };
-    });
-
-    mathematica = let
-      version = "12.1.1";
-    in self.stdenv.mkDerivation {
-      name = "mathematica-${version}";
-      buildCommand = let
-        mathematica = "${self.mathematica-unwrapped}/bin/mathematica";
-      in ''
-        mkdir -p $out/bin
-      '' + builtins.foldl' (l: r: l + ''
-        cat > "$out/bin/${r}" <<EOF
-        #! ${self.runtimeShell} -e
-        export USE_WOLFRAM_LD_LIBRARY_PATH=1
-        export QT_XCB_GL_INTEGRATION=none
-        exec /run/wrappers/bin/firejail --noprofile --net=none "${self.mathematica-unwrapped}/bin/${r}" "\$@"
-        EOF
-        chmod 0755 "$out/bin/${r}" 
-      '') "" [ "math" "mathematica" "Mathematica" "MathKernel" "mcc" "wolfram" "WolframKernel" ];
+    src = self.fetchgit {
+      url = "https://git.code.sf.net/p/gnuplot/gnuplot-main";
+      rev = "ef315e6f148bb3144726246cbf396aad53e3a000";
+      sha256 = "143d3hbw8pk2pp3rpplr81ax5cbcrrfhz994zzky5xavdw4z43df";
     };
-  })
-]
+
+    nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+      self.autoconf
+      self.automake
+      self.git
+    ];
+
+    postPatch = ''
+      ./prepare
+      ${oldAttrs.postPatch}
+    '';
+  });
+
+  mathematica-unwrapped = let
+    version = "12.1.1";
+    name = "Mathematica_${version}_LINUX.sh";
+    sha256 = "02mk8gmv8idnakva1nc7r7mx8ld02lk7jgsj1zbn962aps3bhixd";
+  in unstable.mathematica.overrideAttrs (oldAttrs: {
+    name = "mathematica-unwrapped-${version}";
+    inherit version;
+    src = super.requireFile {
+      inherit name sha256;
+      message = ''
+        This nix expression requires that ${name} is
+        already part of the store. Find the file on your Mathematica CD
+        and add it to the nix store with nix-store --add-fixed sha256 <FILE>.
+      '';
+    };
+  });
+
+  mathematica = let
+    version = "12.1.1";
+  in self.stdenv.mkDerivation {
+    name = "mathematica-${version}";
+    buildCommand = let
+      mathematica = "${self.mathematica-unwrapped}/bin/mathematica";
+    in ''
+      mkdir -p $out/bin
+    '' + builtins.foldl' (l: r: l + ''
+      cat > "$out/bin/${r}" <<EOF
+      #! ${self.runtimeShell} -e
+      export USE_WOLFRAM_LD_LIBRARY_PATH=1
+      export QT_XCB_GL_INTEGRATION=none
+      exec /run/wrappers/bin/firejail --noprofile --net=none "${self.mathematica-unwrapped}/bin/${r}" "\$@"
+      EOF
+      chmod 0755 "$out/bin/${r}"
+    '') "" [ "math" "mathematica" "Mathematica" "MathKernel" "mcc" "wolfram" "WolframKernel" ];
+  };
+
+  softmaker-office = let
+    version = "976";
+    edition = "2018";
+  in self.callPackage <nixpkgs/pkgs/applications/office/softmaker/generic.nix> {
+    pname = "softmaker-office";
+    inherit version edition;
+    suiteName = "SoftMaker Office";
+    src = self.fetchurl {
+      url = "https://www.softmaker.net/down/softmaker-office-${edition}-${version}-amd64.tgz";
+      sha256 = "0j6zm0cbxrcgm7glk84hvvbp4z0ys6v8bkwwhl5r7dbphyi72fw8";
+    };
+    archive = "office${edition}.tar.lzma";
+  };
+}) ]
