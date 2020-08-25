94184b7e833966efb509625439452d6a4e564686
Henri Menke <henrimenke@gmail.com>
2020-08-26 10:05:45 +1200

    [nix] splice in nixpkgs; no helpers in versions file

--- .config/nixpkgs/buildEnv.nix
+++ .config/nixpkgs/buildEnv.nix
@@ -26,133 +26,139 @@ with pkgs;
 buildEnv rec {
   inherit name;
   extraOutputsToInstall = [ "bin" "lib" "out" ];
-  paths = [
-    # command line utils
-    aspell
-    aspellDicts.de
-    aspellDicts.en
-    cachix
-    direnv
-    emacs
-    ffmpeg
-    fzf
-    (hiPrio gcc)
-    gnuplotGit
-    isync
-    llvmPackages_latest.clang
-    msmtp
-    neomutt
-    neovim
-    newsboat
-    niv
-    nix-direnv
-    p7zip
-    pandoc
-    pass-otp
-    qpdf
-    shadowsocks-libev
-    tree
-    ts
-    unison
-    unzip
-    #v2ray-plugin
-    valgrind
-    youtube-dl
-    zip
-  ] ++ [
-    # GUI
-    arandr
-    clementine
-    dunst
-    evince
-    feh
-    firefox
-    gnome3.adwaita-icon-theme
-    libnotify
-    networkmanagerapplet
-    pavucontrol
-    polybarFull
-    rofi
-    scrot
-    vlc
-    xclip
-    xorg.setxkbmap
-    xorg.xbacklight
-    xorg.xmodmap
-    xorg.xsetroot
-    xscreensaver
-  ] ++ lib.lists.optionals allowUnfree [
-    # Proprietary
-    dropbox
-    google-chrome
-    mathematica
-    skypeforlinux
-    softmaker-office
-    zoom-us
-  ] ++
-  # Local package specifications (if they exist)
-  (if builtins.pathExists ./local.nix then import ./local.nix pkgs else []) ++
-  # Helpers for declarative package management
-  [
-    # Script to rebuild the environment from this file.
-    (writeShellScriptBin "nix-rebuild" ''
-      set -e
+  paths = let
 
-      oldGeneration=$(readlink "$(readlink ~/.nix-profile)" | cut -d '-' -f 2)
-      oldVersions=$(readlink ~/.nix-profile/package-versions || echo "/dev/null")
+    localPackages = if builtins.pathExists ./local.nix
+                    then import ./local.nix pkgs
+                    else [];
 
-      ${nix}/bin/nix-env --set -f ~/.config/nixpkgs/buildEnv.nix \
-      --argstr name "$(whoami)-user-env-$(date -I)" \
-      --arg allowUnfree ${lib.trivial.boolToString allowUnfree} \
-      "$@"
+    userPackages = localPackages ++ [
+      # command line utils
+      aspell
+      aspellDicts.de
+      aspellDicts.en
+      cachix
+      direnv
+      emacs
+      ffmpeg
+      fzf
+      (hiPrio gcc)
+      gnuplotGit
+      isync
+      llvmPackages_latest.clang
+      msmtp
+      neomutt
+      neovim
+      newsboat
+      niv
+      nix-direnv
+      p7zip
+      pandoc
+      pass-otp
+      qpdf
+      shadowsocks-libev
+      tree
+      ts
+      unison
+      unzip
+      #v2ray-plugin
+      valgrind
+      youtube-dl
+      zip
+    ] ++ [
+      # GUI
+      arandr
+      clementine
+      dunst
+      evince
+      feh
+      firefox
+      gnome3.adwaita-icon-theme
+      libnotify
+      networkmanagerapplet
+      pavucontrol
+      polybarFull
+      rofi
+      scrot
+      vlc
+      xclip
+      xorg.setxkbmap
+      xorg.xbacklight
+      xorg.xmodmap
+      xorg.xsetroot
+      xscreensaver
+    ] ++ lib.lists.optionals allowUnfree [
+      # Proprietary
+      dropbox
+      google-chrome
+      mathematica
+      skypeforlinux
+      softmaker-office
+      zoom-us
+    ];
 
-      newGeneration=$(readlink "$(readlink ~/.nix-profile)" | cut -d '-' -f 2)
-      newVersions=$(readlink ~/.nix-profile/package-versions || echo "/dev/null")
+    buildEnvHelpers = [
+      # Script to rebuild the environment from this file.
+      (writeShellScriptBin "nix-rebuild" ''
+        set -e
 
-      ${diffutils}/bin/diff --color -u \
-      --label "generation $oldGeneration" "$oldVersions" \
-      --label "generation $newGeneration" "$newVersions" \
-      || true
-    '')
+        oldGeneration=$(readlink "$(readlink ~/.nix-profile)" | cut -d '-' -f 2)
+        oldVersions=$(readlink ~/.nix-profile/package-versions || echo "/dev/null")
 
-    # Manifest to make sure imperative nix-env doesn't work (otherwise it will
-    # overwrite the profile, removing all packages other than the
-    # newly-installed one).
-    (writeTextFile {
-      name = "break-nix-env-manifest";
-      destination = "/manifest.nix";
-      text = ''
-        throw ''\''
-          Your user environment is a buildEnv which is incompatible with
-          nix-env's built-in env builder. Edit your home expression and run
-          nix-rebuild instead!
-        ''\''
-      '';
-    })
+        ${nix}/bin/nix-env --set -f ~/.config/nixpkgs/buildEnv.nix \
+        --argstr name "$(whoami)-user-env-$(date -I)" \
+        --arg allowUnfree ${lib.trivial.boolToString allowUnfree} \
+        "$@"
 
-    # To allow easily seeing which nixpkgs version the profile was built from,
-    # place the version string in ~/.nix-profile/nixpkgs-version
-    (writeTextFile {
-      name = "nixpkgs-version";
-      destination = "/nixpkgs-version";
-      text = lib.version;
-    })
+        newGeneration=$(readlink "$(readlink ~/.nix-profile)" | cut -d '-' -f 2)
+        newVersions=$(readlink ~/.nix-profile/package-versions || echo "/dev/null")
 
-    # Since you can't see the versions with nix-env -q anymore, we write them
-    # to a file for easy querying
-    (let
-       collect = pkgs:
-         let recurse = x:
-           if lib.isDerivation x then [x]
-           else if x.recurseForDerivations or false then collect x
-           else [];
-         in lib.concatMap recurse pkgs;
-       versions = map (pkg: pkg.name) (collect paths);
-       versionText = lib.strings.concatMapStrings (s: s+"\n") versions;
-     in writeTextFile {
-       name = "package-versions";
-       destination = "/package-versions";
-       text = versionText;
-     })
-  ];
+        ${diffutils}/bin/diff --color -u \
+        --label "generation $oldGeneration" "$oldVersions" \
+        --label "generation $newGeneration" "$newVersions" \
+        || true
+      '')
+
+      # Manifest to make sure imperative nix-env doesn't work (otherwise it will
+      # overwrite the profile, removing all packages other than the
+      # newly-installed one).
+      (writeTextFile {
+        name = "break-nix-env-manifest";
+        destination = "/manifest.nix";
+        text = ''
+          throw ''\''
+            Your user environment is a buildEnv which is incompatible with
+            nix-env's built-in env builder. Edit your home expression and run
+            nix-rebuild instead!
+          ''\''
+        '';
+      })
+
+      # Create a link to the nixpkgs that are used to build the environment.
+      # Then it is possible to reference this when using nix imperatively without
+      # having to redownload the tarball, e.g.
+      #
+      #     nix run -f ~/.nix-profile/nixpkgs hello -c hello
+      #
+      (linkFarm "nixpkgs" [ { name = "nixpkgs"; path = nixexprs; } ])
+
+      # Since you can't see the versions with nix-env -q anymore, we write them
+      # to a file for easy querying
+      (let
+         collect = pkgs:
+           let recurse = x:
+             if lib.isDerivation x then [x]
+             else if x.recurseForDerivations or false then collect x
+             else [];
+           in lib.concatMap recurse pkgs;
+         versions = map (pkg: pkg.name) (collect userPackages);
+         versionText = lib.strings.concatMapStrings (s: s+"\n") versions;
+       in writeTextFile {
+         name = "package-versions";
+         destination = "/package-versions";
+         text = versionText;
+       })
+    ];
+
+  in userPackages ++ buildEnvHelpers;
 }
