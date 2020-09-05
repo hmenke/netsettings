37ae11ea0c79b2686a81e7568840554fac8a0680
Henri Menke <henrimenke@gmail.com>
2020-09-05 18:19:02 +1200

    [nix] add splatmoji

--- .config/nixpkgs/buildEnv.nix
+++ .config/nixpkgs/buildEnv.nix
@@ -83,6 +83,7 @@ buildEnv rec {
       polybarFull
       rofi
       scrot
+      splatmoji
       vlc
       xclip
       xorg.setxkbmap
--- .config/nixpkgs/overlays/newPackages.nix
+++ .config/nixpkgs/overlays/newPackages.nix
@@ -43,4 +43,34 @@ self: super: {
       maintainers = with maintainers; [ hmenke ];
     };
   };
+
+  splatmoji = self.stdenv.mkDerivation rec {
+    pname = "splatmoji";
+    version = "1.1.0";
+
+    src = self.fetchFromGitHub {
+      owner = "cspeterson";
+      repo = "splatmoji";
+      rev = "v${version}";
+      sha256 = "0fdnz290a0dp7adrl64pqcppr419x9xibh6pspcjyxwxp7h66a3x";
+    };
+
+    buildInputs = with self; [
+      bashInteractive
+      rofi
+      xdotool
+      xsel
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin $out/share/splatmoji
+      cp -r * $out/share/splatmoji
+      cat <<EOF > $out/share/splatmoji/splatmoji.config
+      rofi_command=${self.rofi}/bin/rofi -dmenu -p "" -i -theme gruvbox-dark
+      xsel_command=${self.xsel}/bin/xsel -b -i
+      xdotool_command=${self.xdotool}/bin/xdotool sleep 0.2 type --delay 100
+      EOF
+      ln -s $out/share/splatmoji/splatmoji $out/bin/splatmoji
+    '';
+  };
 }
