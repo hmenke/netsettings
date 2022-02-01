349fbe300e6a0dbff0ecd2f822639be756bc8445
Henri Menke <henri@henrimenke.de>
2022-02-01 08:33:48 +0100

    [emacs] speed up disabling of useless eyecandy

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,9 +1,19 @@
 ;; -*- lexical-binding: t; -*-
 
-;; disable useless bars
-(when (bound-and-true-p tool-bar-mode) (tool-bar-mode -1))
-(when (bound-and-true-p menu-bar-mode) (menu-bar-mode -1))
-(when (bound-and-true-p scroll-bar-mode) (scroll-bar-mode -1))
+;; disable useless bars (from Doom Emacs)
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+;; disable tooltips (from Doom Emacs)
+(setq use-dialog-box nil)
+(when (bound-and-true-p tooltip-mode)
+  (tooltip-mode -1))
+(when (eq system-type 'gnu/linux)
+  (setq x-gtk-use-system-tooltips nil))
 
 ;; package
 (setq package-enable-at-startup nil)
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -59,11 +59,20 @@ is already narrowed."
 ;; copy it if that's what you want.
 (define-key ctl-x-map "n" #'narrow-or-widen-dwim)
 
-;; disable useless bars
-(when (bound-and-true-p tool-bar-mode) (tool-bar-mode -1))
-(when (bound-and-true-p menu-bar-mode) (menu-bar-mode -1))
-(when (bound-and-true-p scroll-bar-mode) (scroll-bar-mode -1))
-(tooltip-mode 0)
+;; disable useless bars (from Doom Emacs)
+(push '(menu-bar-lines . 0)   default-frame-alist)
+(push '(tool-bar-lines . 0)   default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(setq menu-bar-mode nil
+      tool-bar-mode nil
+      scroll-bar-mode nil)
+
+;; disable tooltips (from Doom Emacs)
+(setq use-dialog-box nil)
+(when (bound-and-true-p tooltip-mode)
+  (tooltip-mode -1))
+(when (eq system-type 'gnu/linux)
+  (setq x-gtk-use-system-tooltips nil))
 
 ;; Enable some disabled commands
 (put 'upcase-region 'disabled nil)
