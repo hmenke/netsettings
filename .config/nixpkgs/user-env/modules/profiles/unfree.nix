4dc30174ab65de1339008efe558f2dcdab2804e8
Henri Menke <henri@henrimenke.de>
2022-09-23 23:11:50 +0200

    [nix] Discord too old again

--- .config/nixpkgs/user-env/modules/profiles/unfree.nix
+++ .config/nixpkgs/user-env/modules/profiles/unfree.nix
@@ -2,17 +2,24 @@
 
 {
   userPackages = with pkgs; [
-    (discord.overrideAttrs (_: lib.optionalAttrs (lib.versionOlder discord.version "0.0.18") rec {
+    (discord.overrideAttrs (_: lib.optionalAttrs (lib.versionOlder discord.version "0.0.20") rec {
       pname = "discord";
-      version = "0.0.18";
+      version = "0.0.20";
       name = "${pname}-${version}";
       src = fetchurl {
         url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-        sha256 = "1hl01rf3l6kblx5v7rwnwms30iz8zw6dwlkjsx2f1iipljgkh5q4";
+        sha256 = "3f7yuxigEF3e8qhCetCHKBtV4XUHsx/iYiaCCXjspYw=";
       };
     }))
     masterpdfeditor4
     slack
+    (softmaker-office.override {
+      officeVersion = {
+        version = "976";
+        edition = "2018";
+        hash = "sha256-A45q/irWxKTLszyd7Rv56WeqkwHtWg4zY9YVxqA/KmQ=";
+      };
+    })
     zoom-us
   ];
 }
