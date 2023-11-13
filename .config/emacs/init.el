hmenke/netsettings

commit 229ddfa3777a027b2c4a5733bc87653d043bf23f

[emacs] vc-git-root missing in older Emacs

.config/emacs/init.el
@@ -483,7 +483,8 @@ is already narrowed."
    (progn
      (grep-compute-defaults)
      (list (grep-read-regexp))))
-  (vc-git-grep regexp "" (vc-git-root default-directory)))
+  (let (root (vc-git-root default-directory))
+    (vc-git-grep regexp "" (if root root default-directory))))
 (global-set-key (kbd "C-x v f") 'user/vc-git-grep)
 
 ;; grep

commit a417c47692344dd42007740d27a11738ab57e6c2

[emacs] fix let expression in vc-git-grep
Bug introduced in 229ddfa3777a027b2c4a5733bc87653d043bf23f

.config/emacs/init.el
@@ -487,7 +487,7 @@ is already narrowed."
    (progn
      (grep-compute-defaults)
      (list (grep-read-regexp))))
-  (let (root (vc-git-root default-directory))
+  (let ((root (vc-git-root default-directory)))
     (vc-git-grep regexp "" (if root root default-directory))))
 (global-set-key (kbd "C-x v f") 'user/vc-git-grep)
 